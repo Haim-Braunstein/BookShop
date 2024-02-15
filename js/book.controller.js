300674338e10c1af6049892b6a21a52d1e4831ab
Haim Braunstein <haim321@gmail.com>
2024-02-15 15:48:07 +0200

    Implement renderBooks()

--- js/book.controller.js
+++ js/book.controller.js
@@ -6,5 +6,23 @@ function onInit() {
 
 function renderBooks() {
 
+    const books = getBooks()
+
+    const strHTMLs = books.map(book => `
+    <tr>
+    <td>${book.title}</td>
+    <td>${book.price}</td>
+    <td>
+        <button>read</button>
+        <button> update</button>
+        <button>delete</button>
+    </td>
+    </tr>
+
+    `)
+
+    const elTable = document.querySelector('.board')
+    elTable.innerHTML = strHTMLs.join('')
+
 
 }
--- js/book.service.js
+++ js/book.service.js
@@ -4,21 +4,21 @@ function getBooks() {
 
     return [
         {
-            id: bg4J78,
+            id: 'bg4J78',
             title: 'The adventures of Lori Ipsi',
             price: 120,
             imgUrl: 'lori-ipsi.jpg'
         },
 
         {
-            id: Fd6J98,
+            id: 'Fd6J98',
             title: 'World Atlas',
             price: 300,
             imgUrl: 'lori-ipsi.jpg'
         },
 
         {
-            id: az27L2,
+            id: 'az27L2',
             title: 'Zorba the Greek',
             price: 87,
             imgUrl: 'lori-ipsi.jpg'
