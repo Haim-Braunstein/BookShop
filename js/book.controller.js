069e6934c3ebd6c77df7b0ae2ab6a8cf8aeeb53f
Haim Braunstein <haim321@gmail.com>
2024-02-15 15:37:09 +0200

    Add js files book.controller and book.service

---
+++ js/book.controller.js
@@ -0,0 +1,10 @@
+
+
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+
+
+}
---
+++ js/book.service.js
@@ -0,0 +1,30 @@
+
+
+function getBooks() {
+
+    return [
+        {
+            id: bg4J78,
+            title: 'The adventures of Lori Ipsi',
+            price: 120,
+            imgUrl: 'lori-ipsi.jpg'
+        },
+
+        {
+            id: Fd6J98,
+            title: 'World Atlas',
+            price: 300,
+            imgUrl: 'lori-ipsi.jpg'
+        },
+
+        {
+            id: az27L2,
+            title: 'Zorba the Greek',
+            price: 87,
+            imgUrl: 'lori-ipsi.jpg'
+        },
+
+
+
+    ]
+}
